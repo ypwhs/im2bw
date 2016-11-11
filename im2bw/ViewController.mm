a44385879c924f7a5358991af00053d6e6c45deb
ypw <915505626@qq.com>
2016-11-11 15:44:24 +0800

    ofo

--- im2bw/ViewController.mm
+++ im2bw/ViewController.mm
@@ -37,10 +37,6 @@ KerasModel * model;
     
     NSString* file_path = [[NSBundle mainBundle] pathForResource:@"model" ofType:@"txt"];
     model = new KerasModel([file_path UTF8String]);
-    
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-        while(true) [self process];
-    });
 }
 
 - (void)viewDidAppear:(BOOL)animated {
@@ -96,8 +92,7 @@ int predict(cv::Mat img){   //预测数字 conv
     dc->set_data(data); //Mat 转 DataChunk
     
     vector<float> predictions = model->compute_output(dc);
-//    predictions[10] = 0;
-
+    
     auto max = max_element(predictions.begin(), predictions.end());
     int index = (int)distance(predictions.begin(), max);
     return index;
@@ -112,12 +107,11 @@ int predict(cv::Mat img){   //预测数字 mlp
             data.push_back(img.at<uchar>(i, j)/255.0);
         }
     }
-
+    
     DataChunk * dc = new DataChunkFlat();
     dc->set_data(data); //Mat 转 DataChunk
     
     vector<float> predictions = model->compute_output(dc);
-//    predictions[10] = 0;
     
     auto max = max_element(predictions.begin(), predictions.end());
     int index = (int)distance(predictions.begin(), max);
@@ -125,73 +119,37 @@ int predict(cv::Mat img){   //预测数字 mlp
 }
 #endif
 
-cv::Mat img;
-cv::Mat gray, bw;
-cv::Mat mask(640, 480, CV_8UC3, cv::Scalar(0,0,0));
-cv::Mat mask_out(640, 480, CV_8UC3, cv::Scalar(0,0,0));
-
-bool newimage = false;
-NSLock * newimagelock = [[NSLock alloc] init];
-NSLock * masklock = [[NSLock alloc] init];
-
-cv::Mat hsv, yellow;
-- (void) process {
-    usleep(1000);
-    if(!newimage) return;
-    [newimagelock lock];
-//    cv::cvtColor(img, gray, CV_BGR2GRAY);
+- (void)processImage:(cv::Mat &)image {
+    cv::Mat img, gray, bw, hsv;
+    cv::cvtColor(image, img, CV_RGBA2RGB);
     cv::cvtColor(img, hsv, CV_BGR2HSV);
-    [newimagelock unlock];
-    cv::inRange(hsv, cv::Scalar(15, 50, 128), cv::Scalar(35, 255, 255), bw);
-
+    cv::inRange(hsv, cv::Scalar(10, 60, 200), cv::Scalar(30, 255, 255), bw);
+//    cvtColor(image, gray, CV_BGR2GRAY);
 //    adaptiveThreshold(gray, bw, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 25, 25);
     
     vector<vector<cv::Point>> rects;
     findContours(bw.clone(), rects, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
     cv::bitwise_not(bw, bw);
     
-    mask = cv::Scalar(0,0,0);
     for( int i = 0; i< rects.size(); i++ ){
         cv::Rect rect = boundingRect(rects[i]);
+        
         int x = rect.x, y = rect.y;
         int w = rect.width, h = rect.height;
         float hw = float(h) / w;
-        if( w < 200 && h < 200 && h > 10 && 1.1 < hw &&  hw < 5) {
+        if( w < 50 && h < 50 && h > 10 && 1.1 < hw &&  hw < 5) {
             cv::Mat res = [self resize:bw(rect)];
-            cv::rectangle(mask, rect, cv::Scalar(0, 255, 0), 0.5);
+            cv::rectangle(image, rect, cv::Scalar(0, 255, 0, 255), 0.5);
             int index = predict(res);
             if(index != 10){
                 char tmp[10];
                 sprintf(tmp, "%d", index);
-                cv::putText(mask, tmp, cv::Point(x, y), cv::FONT_HERSHEY_DUPLEX, 0.5, cv::Scalar(0, 0, 255));
+                cv::putText(image, tmp, cv::Point(x, y), cv::FONT_HERSHEY_DUPLEX, 0.5, cv::Scalar(0, 255, 0, 255));
             }
         }
     }
-    newimage = false;
-    
-    [masklock lock];
-    mask_out = mask.clone();
-    [masklock unlock];
-}
-
-cv::Mat mask_not;
-- (void)processImage:(cv::Mat &)image {
-    cv::cvtColor(image, image, CV_RGBA2BGR);
-    [newimagelock lock];
-    img = image.clone();
-    [newimagelock unlock];
-    newimage = true;
-    
-    [masklock lock];
-    cv::cvtColor(mask_out, mask_not, cv::COLOR_BGR2GRAY);
-    cv::threshold(mask_not, mask_not, 10, 255, cv::THRESH_BINARY);
-    mask_out.copyTo(image, mask_not);
-    [masklock unlock];
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
-        UIImage * outimage = MatToUIImage(img);
-        _imageView.image = outimage;
-    });
+//    cv::cvtColor(bw, img, CV_GRAY2BGR);
+//    image = img;
 }
 
 - (void)didReceiveMemoryWarning {
